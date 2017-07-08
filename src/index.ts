35b5c04680b1229f6b43789d539f0229d0b867c5
Zoltan Kochan <zoltan.kochan@gmail.com>
2017-07-08 17:36:13 +0300

    fix: reused value on Windows

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,32 @@ import os = require('os')
 import mkdirp = require('mkdirp-promise')
 import isWindows = require('is-windows')
 
+const IS_WINDOWS = isWindows()
+
 // Always use "junctions" on Windows. Even though support for "symbolic links" was added in Vista+, users by default
 // lack permission to create them
-const symlinkType = isWindows() ? 'junction' : 'dir'
+const symlinkType = IS_WINDOWS ? 'junction' : 'dir'
+
+const resolveSrc = IS_WINDOWS ? resolveSrcOnWin: resolveSrcOnNonWin
+
+function resolveSrcOnWin (src: string, dest: string) {
+  return `${path.resolve(src)}\\`
+}
+
+function resolveSrcOnNonWin (src: string, dest: string) {
+  return path.relative(path.dirname(dest), path.resolve(src))
+}
 
 async function symlinkDir (src: string, dest: string): Promise<{reused: Boolean}> {
-  src = path.resolve(src)
   dest = path.resolve(dest)
-
-  // Junction points can't be relative
-  const rel = symlinkType !== 'junction' ? path.relative(path.dirname(dest), src) : src
+  src = resolveSrc(src, dest)
 
   try {
-    return await forceSymlink(rel, dest)
+    return await forceSymlink(src, dest)
   } catch (err) {
     if ((<NodeJS.ErrnoException>err).code === 'ENOENT') {
       await mkdirp(path.dirname(dest))
-      return await forceSymlink(rel, dest)
+      return await forceSymlink(src, dest)
     }
     throw err
   }
